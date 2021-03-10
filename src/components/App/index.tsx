85a9ac3d53c4e9da0594d4fc3bc15dcbd24c584f
Laura Munsch <laura.munsch@etu.unistra.fr>
2021-03-10 10:09:04 +0100

    Ajout d'un lien vers l'accueil

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Router } from "react-router-dom";
+import { Link, Route, Router } from "react-router-dom";
 import ListeDePlantes from "../ListeDePlantes";
 import "./App.scss";
 import { createBrowserHistory } from "history";
@@ -12,8 +12,10 @@ const history = createBrowserHistory();
 function App() {
   return (
     <div className="App">
-      <h1>Plantes</h1>
       <Router history={history}>
+        <h1>
+          <Link to="/">Plantes</Link>
+        </h1>
         <Route exact path="/" component={Accueil} />
         <Route exact path="/plante" component={ListeDePlantes} />
         <Route exact path="/plante/:id" component={Plante} />
